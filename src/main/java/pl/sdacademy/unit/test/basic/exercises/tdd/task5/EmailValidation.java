4e075f31f42eb5773af8dbde1a9b589412da39a9
Asati <xxasatixx@gmail.com>
2022-04-04 18:55:52 +0200

    Commit number 1: TestowanieOprogramowaniaPodstawy

--- src/main/java/pl/sdacademy/unit/test/basic/exercises/tdd/task5/EmailValidation.java
+++ src/main/java/pl/sdacademy/unit/test/basic/exercises/tdd/task5/EmailValidation.java
@@ -3,6 +3,7 @@ package pl.sdacademy.unit.test.basic.exercises.tdd.task5;
 public class EmailValidation {
 
     public static boolean validate(String emailStr) {
+        // regex tu bedzie
         // YOUR CODE HERE
         return false;
     }
